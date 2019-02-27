45bab195d72cde589bab2e14fdeee913034d6963
mesadhan <sadhansarker3@gmail.com>
2019-02-27 16:40:19 +0600

    .Net core Web API basic crud done

--- Startup.cs
+++ Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using dotnet_core_webapi_sample.Models;
 
 namespace dotnet_core_webapi_sample
 {
@@ -25,6 +27,8 @@ namespace dotnet_core_webapi_sample
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoContext>(opt =>
+                opt.UseInMemoryDatabase("TodoList"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
