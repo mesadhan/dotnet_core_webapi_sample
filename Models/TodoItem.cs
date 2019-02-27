4c4ac6c4cb6761d4eb47e698052a5a60cbf28c25
mesadhan <sadhansarker3@gmail.com>
2019-02-27 14:25:24 +0600

    crate model and database context

---
+++ Models/TodoItem.cs
@@ -0,0 +1,12 @@
+
+namespace dotnet_core_webapi_sample.Models {
+
+    public class TodoItem
+    {
+       public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+        
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+
+## .Net Core WebAPI Project
+> Create new Project
+
+    dotnet new webapi -o TodoApi
+    
+> Open project in VsCode: 
+    
+    code -r TodoApi
+
+
+> Run dotnet project
+
+    dotnet run
+
+
+
+
