c6189f11e9be3352bf0da4b0e19739daf1f857d4
mesadhan <sadhansarker3@gmail.com>
2019-02-27 16:42:46 +0600

    commented in memory database config scope

--- Startup.cs
+++ Startup.cs
@@ -27,8 +27,12 @@ namespace dotnet_core_webapi_sample
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            // Config In Memory Database
             services.AddDbContext<TodoContext>(opt =>
                 opt.UseInMemoryDatabase("TodoList"));
+        
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
